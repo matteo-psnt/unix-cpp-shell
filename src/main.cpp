ff458e96febab15c8ac868a874d2c5178d192e6e
Matteo_psnt <matteopesenti229@gmail.com>
2025-05-07 23:43:30 -0400

    feat: Add 'echo' command and IDE configuration files
        - Implemented basic functionality for the 'echo' built-in command.
        - Added CLion project configuration files (`.idea` directory).
        - Updated VSCode settings with extensive C++ file associations.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <sstream>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -9,6 +11,19 @@ int main() {
     std::cout << "$ ";
     std::string input;
     std::getline(std::cin, input);
+
+    std::istringstream iss(input);
+    std::vector<std::string> tokens;
+    std::string token;
+    
+    while (iss >> token) {
+      tokens.push_back(token);
+    }
+
+    if (tokens[0] == "exit") {
+      break;
+    } 
+
     std::cout << input << ": command not found" << std::endl;
   }
   return 0;
